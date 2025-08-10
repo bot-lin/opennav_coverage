2d20bad039ece8def38cadd3eb67fa481b03190d
bot-lin <zlin6364@hotmail.com>
2025-08-10 19:20:40 +0800

    Update demo_coverage_http.py

--- opennav_coverage_demo/opennav_coverage_demo/demo_coverage_http.py
+++ opennav_coverage_demo/opennav_coverage_demo/demo_coverage_http.py
@@ -586,7 +586,7 @@ class CoverageNavigatorTester(Node):
                     }
                 },
                 'is_dest': True,
-                'precise_xy': 0.8,
+                'precise_xy': 0.08,
                 'precise_rad': 6.28,
                 'nav_type': 'auto',
                 'actions': [],
